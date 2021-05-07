fcc91b3e48fa4571fd3b36177b40a9affb2cbbee
Marrior-dot <mrlimaroberto@gmail.com>
2021-05-07 14:04:43 -0300

    adicionada a função minsandsecs para contar os segundos e minutos

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var Clock = document.getElementById("minsec");
 var SetTiming = document.getElementById("setter");
 
-SetTiming.onclick = function(setLimit = 25){
+
+function clock(setLimit = 25) {
 setLimit = prompt("Por quanto tempo deseja estudar?");
 parseInt(setLimit);
 
+
+var Rep = setInterval(minandsecs,1000);
 var sec = 60;
+	function minandsecs(){ 
 		sec = sec - 1;
 		if (sec === 0 ){
 			setLimit = setLimit - 1;
 			sec = 60;
 		}
-		console.log(`${setLimit} : ${sec}`)
 		Clock.innerHTML = `<h1> ${setLimit} : ${sec} </h1>`;
 	}
-
+}
