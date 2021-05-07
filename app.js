dd87c9cadb5e42231b828d6c6e92c60d5bf3ccbe
Marrior-dot <mrlimaroberto@gmail.com>
2021-05-07 13:46:11 -0300

    botão start n existe mais, botão set time agr inicia o tempo

--- app.js
+++ app.js
@@ -1,30 +1,17 @@
-var Bstart =document.getElementById("start");
-var Clock = document.getElementById("clock");
-let SetTiming = document.getElementById("setter");
+var Clock = document.getElementById("minsec");
+var SetTiming = document.getElementById("setter");
 
-SetTiming.onclick = function(){
-var setLimit = prompt("Por quanto tempo deseja estudar?");
+SetTiming.onclick = function(setLimit = 25){
+setLimit = prompt("Por quanto tempo deseja estudar?");
 parseInt(setLimit);
-return setLimit;
-
-
-}
-	Bstart.onclick = setInterval(timers.start,1000);
-
 
 var sec = 60;
-var timers = {
-	start(){
-		
 		sec = sec - 1;
 		if (sec === 0 ){
-			timeLimit = it - 1;
+			setLimit = setLimit - 1;
 			sec = 60;
-		}	
-		Clock.innerHTML = `<h1> ${timeLimit} : ${sec} </h1>`;
+		}
+		console.log(`${setLimit} : ${sec}`)
+		Clock.innerHTML = `<h1> ${setLimit} : ${sec} </h1>`;
 	}
-	//pause(){}
-	//break(){}
-}
-
 
